f4b4a764c2570d201f1641d792001ac82cf383bb
Tnixc <tnixxc@gmail.com>
2023-12-17 12:11:23 -0500

    Delete NuxtWelcome component from app.vue

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Inter&display=swap',
+      crossorigin: '',
+    },
+  ],
+})
+</script>
+
+<template>
+  <h1>helo</h1>
+</template>
