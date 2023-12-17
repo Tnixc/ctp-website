297082063f3ed877312c692b5ad59604d4fc16dc
Tnixc <tnixxc@gmail.com>
2023-12-17 17:24:43 -0500

    Update global.css and CopyButton.vue, fix typo in palette.vue

--- components/CopyButton.vue
+++ components/CopyButton.vue
@@ -21,12 +21,12 @@ export default {
 </script>
 
 <template>
-  <button class="flex flex-row items-center gap-x-1 text-xs font-mono" @click="activate">
+  <button class="flex flex-row items-center gap-x-1 text-xs font-mono p-1 bg-surface0 rounded-lg hover:bg-surface1" @click="activate">
     <template v-if="!showCheck">
-      <span class="block w-3 h-3 shrink-0">Copy Icon</span> <!-- Replace with your copy icon -->
+      <span class="block"><svg class="scale-[85%]" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 256 256"><path d="M216,32H88a8,8,0,0,0-8,8V80H40a8,8,0,0,0-8,8V216a8,8,0,0,0,8,8H168a8,8,0,0,0,8-8V176h40a8,8,0,0,0,8-8V40A8,8,0,0,0,216,32ZM160,208H48V96H160Zm48-48H176V88a8,8,0,0,0-8-8H96V48H208Z" /></svg></span>
     </template>
     <template v-else>
-      <span class="block w-3 h-3 shrink-0">Check Icon</span> <!-- Replace with your check icon -->
+      <span class="block"><svg class="scale-[85%]" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 256 256"><path d="M229.66,77.66l-128,128a8,8,0,0,1-11.32,0l-56-56a8,8,0,0,1,11.32-11.32L96,188.69,218.34,66.34a8,8,0,0,1,11.32,11.32Z" /></svg></span>
     </template>
     <span class="whitespace-nowrap">{{ text }}</span>
   </button>
--- pages/palette.vue
+++ pages/palette.vue
@@ -4,7 +4,7 @@ import { variants } from '@catppuccin/palette'
 const flavors = Object.keys(variants) as (keyof typeof variants)[]
 const flavorDisplayName = {
   latte: 'Latte',
-  frappe: 'Frappé',
+  frappe: 'Frappe',
   macchiato: 'Macchiato',
   mocha: 'Mocha',
 }
@@ -23,63 +23,88 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-extrabold tracking-tight mb-3">
-      Palette
-    </h1>
-    <p class="text-lg text-ctp-subtext1 font-medium tracking-tight mb-12">
-      Catppuccin consists of 4 beautiful pastel color palettes, named flavors. If you want to use them
-      for your own project, refer to our
-      <a class="inline-link" href="https://github.com/catppuccin/catppuccin/blob/main/docs/style-guide.md">style guide</a>
-      for general use cases and guidelines. Additionally, you can find integrations with popular frameworks
-      and tools in
-      <a class="inline-link" href="https://github.com/catppuccin/palette">catppuccin/palette</a>.
-    </p>
-
-    <div class="flex flex-col gap-y-6">
-      <div v-for="flavor in flavors" :key="flavor" class="p-8 flex flex-col gap-y-3 rounded-lg bg-ctp-mantle">
-        <h2 class="text-2xl font-bold tracking-tight">
-          {{ flavorDisplayName[flavor] }}
-        </h2>
-
-        <div class="overflow-x-scroll w-full">
+  <div class="bg-base text-text min-h-screen">
+    <div class=" flex flex-col gap-4 items-center p-5">
+      <h1 class="text-3xl font-extrabold tracking-tight mb-3 p-5">
+        Palette
+      </h1>
+      <p class="text-lg text-ctp-subtext1 font-medium tracking-tight mb-12 max-w-4xl p-5 text-subtext1">
+        Catppuccin consists of 4 beautiful pastel color palettes, named flavors. If you want to use them
+        for your own project, refer to our
+        <a class="inline-link" href="https://github.com/catppuccin/catppuccin/blob/main/docs/style-guide.md">style
+          guide</a>
+        for general use cases and guidelines. Additionally, you can find integrations with popular frameworks
+        and tools in
+        <a class="inline-link" href="https://github.com/catppuccin/palette">catppuccin/palette</a>.
+      </p>
+    </div>
+    <div class="flex flex-col gap-y-6 items-center w-full">
+      <details
+        v-for=" flavor in flavors " :key=" flavor "
+        class="p-8 flex flex-col gap-y-3 rounded-lg w-full max-w-4xl bg-crust"
+      >
+        <summary class="text-2xl font-bold cursor-default flex items-center justify-around gap-2">
+          <h2 class="flex-grow">
+            {{ flavorDisplayName[flavor] }}
+          </h2>
+          <div v-for=" colorKey in colors " :key=" colorKey " class="md:block hidden">
+            <span class="block w-4 h-4 rounded-md" :style=" { backgroundColor: variants[flavor][colorKey].hex } " />
+          </div>
+        </summary>
+        <div class="overflow-x-scroll bg-mantle p-4 rounded-lg mt-8">
           <table class="w-full">
-            <tr class="font-medium text-xs text-ctp-subtext0 mb-2">
-              <td class="p-2">
-                Color
+            <tr v-for=" colorKey in colors " :key=" colorKey " class="grid grid-cols-8 place-content-center">
+              <td class="p-2 flex flex-row items-center gap-x-2 col-span-2">
+                <span :style=" { backgroundColor: variants[flavor][colorKey].hex } " class="block w-4 h-4 rounded-full" />
+                <span class="font-semibold text-sm ">{{ colorKey[0].toUpperCase() + colorKey.slice(1) }}</span>
               </td>
               <td class="p-2">
-                Hex
+                <CopyButton :text=" variants[flavor][colorKey].hex " />
+              </td>
+              <td class="p-2 col-span-2">
+                <CopyButton :text=" variants[flavor][colorKey].rgb " />
               </td>
               <td class="p-2">
-                RGB
-              </td>
-              <td class="p-2">
-                HSL
-              </td>
-            </tr>
-
-            <tr v-for="colorKey in colors" :key="colorKey">
-              <td class="p-2 flex flex-row items-center gap-x-2">
-                <span :style="{ backgroundColor: variants[flavor][colorKey].hex }" class="block w-4 h-4 rounded-full" />
-                <span class="font-semibold text-sm">{{ colorKey[0].toUpperCase() + colorKey.slice(1) }}</span>
-              </td>
-
-              <td class="p-2">
-                <CopyButton :text="variants[flavor][colorKey].hex" />
-              </td>
-
-              <td class="p-2">
-                <CopyButton :text="variants[flavor][colorKey].rgb" />
-              </td>
-
-              <td class="p-2">
-                <CopyButton :text="variants[flavor][colorKey].hsl" />
+                <CopyButton :text=" variants[flavor][colorKey].hsl " />
               </td>
             </tr>
           </table>
         </div>
-      </div>
+      </details>
     </div>
   </div>
 </template>
+
+<style>
+.inline-link {
+  text-shadow:
+    -1px -1px 0 var(--base),
+    1px -1px 0 var(--base),
+    -1px 1px 0 var(--base),
+    1px 1px 0 var(--base),
+    0 1px 0 var(--base),
+    0 -1px 0 var(--base),
+    -1px 0 0 var(--base),
+    1px 0 0 var(--base);
+  position: relative;
+  z-index: 3;
+}
+
+.inline-link::after {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: var(--blue);
+  z-index: -1;
+  border-radius: 99px;
+  opacity: 0.75;
+  transition: 0.2s;
+}
+
+.inline-link:hover::after {
+  opacity: 1;
+}
+</style>
