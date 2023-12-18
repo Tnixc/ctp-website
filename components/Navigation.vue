063e2bb8a200408b0b5a1d4f6fb65ab33c205d2f
Tnixc <tnixxc@gmail.com>
2023-12-17 21:34:30 -0500

    Update global.css and Navigation.vue

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -71,6 +71,7 @@ export default {
           <button class="lg:hidden block" @click="toggleVisibility">
             <svg v-show="isOpen" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z" /></svg>
             <svg v-show="!isOpen" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z" /></svg>
+            <span class="sr-only">Toggle Menu</span>
           </button>
         </div>
         <div
