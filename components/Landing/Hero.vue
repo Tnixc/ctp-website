8b1506e8ae05e2aeaa4c80a514bb00960d56f99f
Tnixc <tnixxc@gmail.com>
2023-12-17 21:40:36 -0500

    Update Hero and Navigation components

--- components/Landing/Hero.vue
+++ components/Landing/Hero.vue
@@ -60,9 +60,10 @@ export default {
         Four beautiful flavors
       </h3>
       <div class="flex">
-        <div class="w-6">
+        <div class="w-7">
           <span id="sel" class="text-2xl text-teal block absolute"> -> </span>
         </div>
+        <div class="h-full w-0.5 bg-surface0 rounded-full" />
         <div class="flex flex-col">
           <button class="p-2 text-xl w-fit" @click="setTheme('latte')">
             Latte
@@ -93,19 +94,19 @@ export default {
         },
       } " class="text-2xl font-flow flex flex-col max-w-2xl p-5 bg-mantle h-fit rounded-lg select-none overflow-hidden mt-20"
     >
-      <div v-motion-slide-right :delay="1000" class="line overflow-hidden"><span class="mx-4 text-mauve">for</span> (<span class="mx-4 text-mauve">var</span> i = <span class="hljs-number">1</span>; i &lt;= <span class="hljs-number">5</span>; i++) {</div>
+      <div v-motion-slide-right :delay="1000" class="line overflow-hidden"><span class="mx-4 text-mauve">for</span> (<span class="mx-4 text-mauve">var</span> i<span class="text-teal"> = </span><span class="hljs-number">1</span>; i &lt;= <span class="hljs-number">5</span>; i++) {</div>
       <div v-motion-slide-right :delay="1000" class="line overflow-hidden">    <span class="mx-4 text-peach">console</span>.<span class="mx-4 text-blue">log</span>(<span class="mx-4 text-green">"Count:"</span>, i);</div>
-      <div v-motion-slide-right :delay="1000" class="line overflow-hidden">}</div>
+      <div v-motion-slide-right :delay="1000" class="line overflow-hidden"> }</div>
 
       <div v-motion-slide-left :delay="1000" class="line overflow-hidden"><span class="mx-4 text-overlay0">// Array iteration</span></div>
-      <div v-motion-slide-left :delay="1000" class="line overflow-hidden"><span class="mx-4 text-mauve">var</span> colors = [<span class="mx-4 text-green">"Red"</span>, <span class="mx-4 text-green">"Green"</span>, <span class="mx-4 text-green">"Blue"</span>];</div>
+      <div v-motion-slide-left :delay="1000" class="line overflow-hidden"><span class="mx-4 text-mauve">var</span> colors<span class="text-teal"> = </span>[<span class="mx-4 text-green">"Red"</span>, <span class="mx-4 text-green">"Green"</span>, <span class="mx-4 text-green">"Blue"</span>];</div>
       <div v-motion-slide-left :delay="1000" class="line overflow-hidden"><span class="mx-4 text-mauve">for</span> (<span class="mx-4 text-mauve">var</span> index <span class="mx-4 text-mauve">in</span> colors) {</div>
       <div v-motion-slide-left :delay="1000" class="line overflow-hidden">    <span class="mx-4 text-peach">console</span>.<span class="mx-4 text-blue">log</span>(<span class="mx-4 text-green">"Color:"</span>, colors[index]);</div>
-      <div v-motion-slide-left :delay="1000" class="line overflow-hidden">}</div>
+      <div v-motion-slide-left :delay="1000" class="line overflow-hidden"><span class="text-yellow"> + something </span></div>
 
       <div v-motion-slide-right :delay="1000" class="line overflow-hidden"><span class="mx-4 text-overlay0">// Assuming there is an HTML element with the id "myParagraph"</span></div>
-      <div v-motion-slide-right :delay="1000" class="line overflow-hidden"><span class="mx-4 text-mauve">var</span> paragraph = <span class="mx-4 text-peach">document</span>.<span class="mx-4 text-blue">getElementById</span>(<span class="mx-4 text-green">"myParagraph"</span>);</div>
-      <div v-motion-slide-right :delay="1000" class="line overflow-hidden">paragraph.<span class="hljs-property">innerHTML</span> = <span class="mx-4 text-green">"New content for the paragraph!"</span>;</div>
+      <div v-motion-slide-right :delay="1000" class="line overflow-hidden"><span class="mx-4 text-mauve">var</span><span class="underline decoration-red decoration-8"> paragraph<span class="text-teal"> = </span></span><span class="mx-4 text-peach">document</span>.<span class="mx-4 text-blue">getElementById</span>(<span class="mx-4 text-green">"myParagraph"</span>);</div>
+      <div v-motion-slide-right :delay="1000" class="line overflow-hidden">    paragraph.<span class="hljs-property">innerHTML</span><span class="text-teal"> = </span><span class="mx-4 text-green">"New content for the paragraph!"</span>;</div>
       <div v-motion-slide-right :delay="1000" class="line overflow-hidden" /></pre>
   </div>
 </template>
