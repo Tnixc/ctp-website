41090fe362d145dc57b181224241b6c39b80ed0c
Tnixc <tnixxc@gmail.com>
2023-12-19 22:55:55 -0500

    Refactor parsing logic and add missing type annotation

--- server/ports.ts
+++ server/ports.ts
@@ -1,11 +1,24 @@
 import YAML from 'yaml'
 
-const yamlUrl = 'https://raw.githubusercontent.com/catppuccin/catppuccin/main/resources/ports.yml'
-
 async function fetchAndParseYaml(url) {
   const response = await fetch(url)
   const yamlText = await response.text()
   return YAML.parse(yamlText)
 }
 
-const parsedYaml = await fetchAndParseYaml(yamlUrl)
+function parseNormal(obj: object) {
+  const result = []
+  for (const key in obj.ports) {
+    const port = obj.ports[key]
+    const color = port.color || 'surface1'
+    const link = `https://github.com/catppuccin/${key}`
+    result.push([port.name, port.category, color, link])
+  }
+
+  return result
+}
+
+const parsedYaml = await fetchAndParseYaml('https://raw.githubusercontent.com/catppuccin/catppuccin/main/resources/ports.yml')
+export const something = parseNormal(parsedYaml)
+
+// export const something = await parseNormal(parsedYaml)
