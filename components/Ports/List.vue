a021463c6f4438cd559ad6bacd862d5b55d2781f
Tnixc <tnixxc@gmail.com>
2023-12-19 22:39:13 -0500

    Add YAML parsing for ports.yml

--- components/Ports/List.vue
+++ components/Ports/List.vue
@@ -1,4 +1,13 @@
+<script lang="ts">
+import { something } from '@/server/ports'
+
+export default {
+  mounted() {
+    console.log(something)
+  },
+}
+</script>
+
 <template>
   <div>1</div>
-  
 </template>
--- server/ports.ts
+++ server/ports.ts
@@ -1,1 +1,11 @@
-export const something = 'something'
+import YAML from 'yaml'
+
+const yamlUrl = 'https://raw.githubusercontent.com/catppuccin/catppuccin/main/resources/ports.yml'
+
+async function fetchAndParseYaml(url) {
+  const response = await fetch(url)
+  const yamlText = await response.text()
+  return YAML.parse(yamlText)
+}
+
+const parsedYaml = await fetchAndParseYaml(yamlUrl)
